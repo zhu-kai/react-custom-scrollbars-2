479b5b36c84fe2f6397d883b658aa71ae8e1b0a3
zhu-kai <zhukai1010@gmail.com>
2022-02-18 11:30:05 +1100

    Add console debug

--- src/utils/getScrollbarWidth.js
+++ src/utils/getScrollbarWidth.js
@@ -2,23 +2,25 @@ import css from 'dom-css';
 let scrollbarWidth = false;
 
 export default function getScrollbarWidth(cacheEnabled = true) {
-    if (cacheEnabled && scrollbarWidth !== false) return scrollbarWidth;
-    /* istanbul ignore else */
-    if (typeof document !== 'undefined') {
-        const div = document.createElement('div');
-        css(div, {
-            width: 100,
-            height: 100,
-            position: 'absolute',
-            top: -9999,
-            overflow: 'scroll',
-            MsOverflowStyle: 'scrollbar'
-        });
-        document.body.appendChild(div);
-        scrollbarWidth = (div.offsetWidth - div.clientWidth);
-        document.body.removeChild(div);
-    } else {
-        scrollbarWidth = 0;
-    }
-    return scrollbarWidth || 0;
+  console.log('getScrollbarWidth');
+  if (cacheEnabled && scrollbarWidth !== false) return scrollbarWidth;
+  /* istanbul ignore else */
+  if (typeof document !== 'undefined') {
+    const div = document.createElement('div');
+    css(div, {
+      width: 100,
+      height: 100,
+      position: 'absolute',
+      top: -9999,
+      overflow: 'scroll',
+      MsOverflowStyle: 'scrollbar',
+    });
+    document.body.appendChild(div);
+    scrollbarWidth = div.offsetWidth - div.clientWidth;
+    console.log('scrollbarWidth', scrollbarWidth);
+    document.body.removeChild(div);
+  } else {
+    scrollbarWidth = 0;
+  }
+  return scrollbarWidth || 0;
 }
